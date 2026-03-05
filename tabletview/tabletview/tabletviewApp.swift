96360d408cd0988023a7dc98f41a2fdae192a739
Tommy McClure <tommy.mcclure@fhnw.edu>
2026-03-05 08:33:28 -0700

    chore: added backend & iOS TabletView

---
+++ tabletview/tabletview/tabletviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabletviewApp.swift
+//  tabletview
+//
+//  Created by Tommy McClure on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct tabletviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
